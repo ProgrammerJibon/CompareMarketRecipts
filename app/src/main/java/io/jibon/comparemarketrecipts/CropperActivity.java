144f8c58ff6747f8236ad11b5750e90d962d0a77
Programmer Jibon <programmerjibon@gmail.com>
2022-07-12 23:21:11 +0600

    successfully added imager cropping and edditing

---
+++ app/src/main/java/io/jibon/comparemarketrecipts/CropperActivity.java
@@ -0,0 +1,65 @@
+package io.jibon.comparemarketrecipts;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.yalantis.ucrop.UCrop;
+
+import java.io.File;
+import java.util.UUID;
+
+public class CropperActivity extends AppCompatActivity {
+    Activity activity;
+    String result;
+    Uri fileUril;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cropper);
+        activity = this;
+        readIntent();
+
+        String dest_uri = UUID.randomUUID().toString() + ".jpg";
+        UCrop.Options options = new UCrop.Options();
+        options.setBrightnessEnabled(true);
+        options.setContrastEnabled(true);
+        UCrop.of(fileUril, Uri.fromFile(new File(getCacheDir(), dest_uri)))
+                .withAspectRatio(0, 0)
+                .withMaxResultSize(2048, 2048)
+                .start(CropperActivity.this);
+        
+    }
+
+    private void readIntent() {
+        Intent intent = getIntent();
+        if (intent.getExtras() != null){
+            result = intent.getStringExtra("DATA");
+            fileUril = Uri.parse(result);
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == UCrop.REQUEST_CROP) {
+            final Uri resultUri = UCrop.getOutput(data);
+            Intent returnIntent = new Intent();
+            returnIntent.putExtra("RESULT", resultUri + "");
+            setResult(-1, returnIntent);
+            finish();
+        }else{
+            final Throwable ucrop_error = UCrop.getError(data);
+            assert ucrop_error != null;
+            Toast.makeText(activity, "Something maybe not working\n"+ucrop_error.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- app/src/main/java/io/jibon/comparemarketrecipts/Home.java
+++ app/src/main/java/io/jibon/comparemarketrecipts/Home.java
@@ -1,14 +1,70 @@
 package io.jibon.comparemarketrecipts;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.yalantis.ucrop.UCrop;
+import com.yalantis.ucrop.view.CropImageView;
 
 public class Home extends AppCompatActivity {
+    Button button_capture, button_copy;
+    TextView text_data;
+    Activity activity;
+    ImageView image_crop_view;
+    ActivityResultLauncher<String> mGetContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        activity = this;
+
+        button_capture = activity.findViewById(R.id.button_capture);
+        button_copy = activity.findViewById(R.id.button_copy);
+        text_data = activity.findViewById(R.id.text_data);
+        image_crop_view = activity.findViewById(R.id.image_view_crop);
+
+
+        button_capture.setOnClickListener(view -> {
+            mGetContent.launch("image/*");
+        });
+
+        mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri result) {
+                Intent intent = new Intent(activity, CropperActivity.class);
+                intent.putExtra("DATA", result.toString());
+                startActivityForResult(intent, 10118);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == -1 && requestCode == 10118){
+            String result = data.getStringExtra("RESULT");
+            Uri resultUri = null;
+            if (result != null){
+                resultUri = Uri.parse(result);
+            }
+            image_crop_view.setImageURI(resultUri);
+        }
     }
 }
